1c654dd9f3782d53668a8d2de39ddff0e7bb547f
CaptainBlain <blainellis@hotmail.co.uk>
2021-10-12 16:47:36 +0100

    Created main view header swift UI replacement

---
+++ Crowded/View/SwiftUI/MainViewHeader.swift
@@ -0,0 +1,67 @@
+//
+//  MainViewHeader.swift
+//  Crowded
+//
+//  Created by Blain Ellis on 12/10/2021.
+//
+
+import SwiftUI
+
+struct MainViewHeader: View {
+    
+    @State private var showingSheet = false
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text("Find independant retailers in:")
+                    .font(.body.italic())
+                    .padding(.bottom, 1)
+                
+                Button("Select a city", action: {
+                    showingSheet.toggle()
+                })
+                .font(Font.helveticaBold(size: 26.0))
+                .foregroundColor(Color("textFieldPlaceholder"))
+                .sheet(isPresented: $showingSheet) {
+                    CitySelectSwiftUIView()
+                }
+                
+            }
+            .frame(maxWidth: .infinity,
+                   alignment: .leading)
+            .padding(.all, 10)
+            
+            Button(action: {
+                showingSheet.toggle()
+            })
+            {
+                Image("gear-icon")
+                    .renderingMode(Image.TemplateRenderingMode?.init(Image.TemplateRenderingMode.original))
+            }
+            .sheet(isPresented: $showingSheet) {
+                SettingsSwiftUIView()
+            }
+            
+        }
+        
+    }
+}
+/*
+ Button("Show Sheet") {
+     showingSheet.toggle()
+        }
+        .sheet(isPresented: $showingSheet) {
+         CitySelectSwiftUIView()
+        }
+ 
+ Button("Select a city") {
+ }
+ .font(Font.helveticaBold(size: 26.0))
+ .foregroundColor(Color("textFieldPlaceholder"))*/
+
+struct MainViewHeader_Previews: PreviewProvider {
+    static var previews: some View {
+        MainViewHeader()
+    }
+}
