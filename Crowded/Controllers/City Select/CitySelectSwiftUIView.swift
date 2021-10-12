c24ce5904fe874ae7461a68739d3ae43a68fb28c
CaptainBlain <blainellis@hotmail.co.uk>
2021-10-12 16:48:12 +0100

    Created CitySelect SwiftUI Replacement

---
+++ Crowded/Controllers/City Select/CitySelectSwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  CitySelectSwiftUIView.swift
+//  Crowded
+//
+//  Created by Blain Ellis on 12/10/2021.
+//
+
+import SwiftUI
+
+struct CitySelectSwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CitySelectSwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        CitySelectSwiftUIView()
+    }
+}
