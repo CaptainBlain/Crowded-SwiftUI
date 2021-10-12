7a4c1e293f3922ce7f063c59f2d19da6636ae781
CaptainBlain <blainellis@hotmail.co.uk>
2021-10-12 16:48:27 +0100

    Updated Business Details SwiftUI

---
+++ Crowded/Controllers/Details/BusinessDetailSwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  BusinessDetailView.swift
+//  Crowded
+//
+//  Created by Blain Ellis on 07/10/2021.
+//
+
+import SwiftUI
+
+struct BusinessDetailSwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BusinessDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        BusinessDetailSwiftUIView()
+    }
+}
