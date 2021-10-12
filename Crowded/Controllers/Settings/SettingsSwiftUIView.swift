d0d30ec10812ce2826ebe87d63ade4d5570b7d66
CaptainBlain <blainellis@hotmail.co.uk>
2021-10-12 16:49:11 +0100

    Created Settings SwiftUI Replacement

---
+++ Crowded/Controllers/Settings/SettingsSwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsViewSwiftUI.swift
+//  Crowded
+//
+//  Created by Blain Ellis on 12/10/2021.
+//
+
+import SwiftUI
+
+struct SettingsSwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsViewSwiftUI_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsSwiftUIView()
+    }
+}
